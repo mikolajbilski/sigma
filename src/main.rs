e2f4c404e7ef430d258643392649e07d4c7a5967
Mikolaj Bilski <mj.bilski@student.uw.edu.pl>
2024-12-06 04:34:19 +0100

    simple window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use tetra::graphics::{self, Color};
+use tetra::{Context, ContextBuilder, State};
+
+fn main() -> tetra::Result {
+    ContextBuilder::new("SIGMA", 640, 480)
+        .quit_on_escape(true)
+        .build()?
+        .run(|_| Ok(GameState {}))
+}
+
+struct GameState {}
+
+impl State for GameState {
+    fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
+        graphics::clear(ctx, Color::rgb(0.392, 0.584, 0.929));
+
+        Ok(())
+    }
 }
