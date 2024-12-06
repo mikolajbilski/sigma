efedb77b58aad542c52be6d35b4bffe2cc1c4fff
Mikolaj Bilski <mj.bilski@student.uw.edu.pl>
2024-12-06 04:37:18 +0100

    some default constants

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use tetra::graphics::{self, Color};
 use tetra::{Context, ContextBuilder, State};
 
+const DEFAULT_WINDOW_WIDTH: f32 = 1280.0;
+const DEFAULT_WINDOW_HEIGHT: f32 = 720.0;
+
+const DEFAULT_SCREEN_BACKGROUND: Color = Color::rgb(0.392, 0.584, 0.929);
+
 fn main() -> tetra::Result {
-    ContextBuilder::new("SIGMA", 640, 480)
+    ContextBuilder::new("SIGMA", DEFAULT_WINDOW_WIDTH as i32, DEFAULT_WINDOW_HEIGHT as i32)
         .quit_on_escape(true)
         .build()?
         .run(|_| Ok(GameState {}))
@@ -12,7 +17,7 @@ struct GameState {}
 
 impl State for GameState {
     fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
-        graphics::clear(ctx, Color::rgb(0.392, 0.584, 0.929));
+        graphics::clear(ctx, DEFAULT_SCREEN_BACKGROUND);
 
         Ok(())
     }
