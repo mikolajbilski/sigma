993f09580d9151baec18727d742fa269178e68e1
Mikolaj Bilski <mj.bilski@student.uw.edu.pl>
2024-12-07 04:23:25 +0100

    made the sprite smaller

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use bevy::math::vec3;
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 
@@ -5,27 +6,29 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, spawn_camera)
-        .add_systems(Startup, spawn_player)
+        .add_systems(Startup, spawn_card)
         .run();
 }
 
 #[derive(Component)]
-pub struct Player {}
+pub struct Card {}
 
-pub fn spawn_player(
+pub fn spawn_card(
     mut commands: Commands,
     window_query: Query<&Window, With<PrimaryWindow>>,
     asset_server: Res<AssetServer>,
 ) {
     let window = window_query.get_single().unwrap();
 
+    //let mut sprite: Handle<Image> = asset_server.load("sprites/diamond.png");
+
     commands.spawn((
         SpriteBundle {
-            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0).with_scale(vec3(0.5, 0.5, 1.0)),
             texture: asset_server.load("sprites/diamond.png"),
             ..default()
         },
-        Player {},
+        Card {},
     ));
 }
 
