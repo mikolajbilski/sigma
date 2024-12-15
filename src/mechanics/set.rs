5fd8d370bdeb9ac3794d474a5facdb48221329fe
Mikolaj Bilski <mj.bilski@student.uw.edu.pl>
2024-12-15 23:26:08 +0100

    better set debug message

--- src/mechanics/set.rs
+++ src/mechanics/set.rs
@@ -19,7 +19,7 @@ pub fn is_set(card1: &Card, card2: &Card, card3: &Card) -> bool {
         return false;
     }
 
-    println!("SET: {:?}, {:?}, {:?}", card1, card2, card3);
+    println!("SET:\n{:?},\n{:?},\n{:?}", card1, card2, card3);
 
     true
 }
