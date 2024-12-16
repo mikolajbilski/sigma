43ce6d40294a589dd593e64bbd2092f13cbca93a
Mikolaj Bilski <mj.bilski@student.uw.edu.pl>
2024-12-16 02:03:16 +0100

    reorganize order of methods for better readability

--- src/mechanics/playing_field.rs
+++ src/mechanics/playing_field.rs
@@ -46,18 +46,6 @@ pub(crate) fn display(
     }
 }
 
-fn card_id_to_pos(id: usize) -> (f32, f32, f32) {
-    const COLUMN_X: &[f32] = &[-200.0, 0.0, 200.0];
-    // rows are displayed top to bottom, apart from the *REALLY* rare last row
-    const ROW_Y: &[f32] = &[180.0, 60.0, -60.0, -180.0, -300.0, -420.0, 300.0];
-
-    if id > 20 {
-        panic!("Unexpected ID!");
-    }
-
-    (640.0 + COLUMN_X[id % 3], 450.0 + ROW_Y[id / 3], 0.0)
-}
-
 // Move the sprites of the cards to "fill" empty spaces
 pub(crate) fn move_to_compress(
     mut field_query: Query<&mut GameManager>,
@@ -111,9 +99,55 @@ pub(crate) fn remove_found_set(
     }
 }
 
+fn card_id_to_pos(id: usize) -> (f32, f32, f32) {
+    const COLUMN_X: &[f32] = &[-200.0, 0.0, 200.0];
+    // rows are displayed top to bottom, apart from the *REALLY* rare last row
+    const ROW_Y: &[f32] = &[180.0, 60.0, -60.0, -180.0, -300.0, -420.0, 300.0];
+
+    if id > 20 {
+        panic!("Unexpected ID!");
+    }
+
+    (640.0 + COLUMN_X[id % 3], 450.0 + ROW_Y[id / 3], 0.0)
+}
+
 impl PlayingField {
     pub(crate) fn new() -> Self {
         PlayingField { cards: vec![] }
+    }
+
+    pub(crate) fn remove_cards(&mut self, to_remove: Vec<Card>) {
+        for card in self.cards.iter_mut() {
+            if let Some(internal) = card {
+                if to_remove.contains(internal) {
+                    *card = None;
+                }
+            }
+        }
+    }
+
+    // "compress" the playing field, removing any None spaces
+    // this will take effect mostly at the end of the game, when there aren't any cards in the deck
+    // it will also take effect when we had more than 12 cards on the field at some point because there were no sets
+    fn compress(&mut self) {
+        // TODO: update the transforms of the cards
+        self.cards.retain(|card| card.is_some());
+    }
+
+    pub(crate) fn add_cards(&mut self, mut added: Vec<Card>) {
+        // Add cards, beginning with filling the empty spaces and then appending to the end of the vector
+        for card in self.cards.iter_mut() {
+            if card.is_some() {
+                continue;
+            }
+            if let Some(new_card) = added.pop() {
+                *card = Some(new_card);
+            }
+        }
+
+        self.compress();
+
+        self.cards.extend(added.into_iter().map(Some));
     }
 
     pub(crate) fn contains_set(&self) -> bool {
@@ -143,16 +177,6 @@ impl PlayingField {
         false
     }
 
-    pub(crate) fn remove_cards(&mut self, to_remove: Vec<Card>) {
-        for card in self.cards.iter_mut() {
-            if let Some(internal) = card {
-                if to_remove.contains(internal) {
-                    *card = None;
-                }
-            }
-        }
-    }
-
     pub(crate) fn cards_count(&self) -> usize {
         self.cards.iter().filter(|card| card.is_some()).count()
     }
@@ -163,29 +187,5 @@ impl PlayingField {
 
     pub(crate) fn get_cards_mut(&mut self) -> Vec<&mut Option<Card>> {
         self.cards.iter_mut().collect()
-    }
-
-    // "compress" the playing field, removing any None spaces
-    // this will take effect mostly at the end of the game, when there aren't any cards in the deck
-    // it will also take effect when we had more than 12 cards on the field at some point because there were no sets
-    fn compress(&mut self) {
-        // TODO: update the transforms of the cards
-        self.cards.retain(|card| card.is_some());
-    }
-
-    pub(crate) fn add_cards(&mut self, mut added: Vec<Card>) {
-        // Add cards, beginning with filling the empty spaces and then appending to the end of the vector
-        for card in self.cards.iter_mut() {
-            if card.is_some() {
-                continue;
-            }
-            if let Some(new_card) = added.pop() {
-                *card = Some(new_card);
-            }
-        }
-
-        self.compress();
-
-        self.cards.extend(added.into_iter().map(Some));
     }
 }
