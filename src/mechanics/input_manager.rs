34445e729240226ab7f2581c2a36b246fd8df780
Mikolaj Bilski <mj.bilski@student.uw.edu.pl>
2024-12-16 01:32:58 +0100

    refactoring and error handling in processing mouse clicks

--- src/mechanics/input_manager.rs
+++ src/mechanics/input_manager.rs
@@ -7,24 +7,28 @@ pub fn handle_mouse_clicks(
     window_query: Query<&Window, With<PrimaryWindow>>,
     mut card_query: Query<(&mut Card, &Transform)>,
 ) {
-    let win = window_query.get_single().unwrap();
     if mouse_input.just_pressed(MouseButton::Left) {
-        if let Some(pos) = win.cursor_position() {
-            let click_x = pos[0];
-            // transformation is required because world has y-axis increasing upwards, and window the opposite
-            let click_y = 900.0 - pos[1];
-            for mut card in &mut card_query {
-                let card_x = card.1.translation[0];
-                let card_y = card.1.translation[1];
-                if click_x > card_x - 90.0
-                    && click_x < card_x + 90.0
-                    && click_y > card_y - 54.0
-                    && click_y < card_y + 54.0
-                {
-                    // TODO: highlight the card
-                    card.0.flip_selection();
+        if let Ok(win) = window_query.get_single() {
+            if let Some(pos) = win.cursor_position() {
+                // Calculating click_y is required because world has y-axis increasing upwards, and window the opposite
+                let click_x = pos[0];
+                let click_y = 900.0 - pos[1];
+
+                for mut card in &mut card_query {
+                    let card_x = card.1.translation[0];
+                    let card_y = card.1.translation[1];
+                    if click_x > card_x - 90.0
+                        && click_x < card_x + 90.0
+                        && click_y > card_y - 54.0
+                        && click_y < card_y + 54.0
+                    {
+                        // TODO: highlight the card
+                        card.0.flip_selection();
+                    }
                 }
             }
+        } else {
+            panic!("No window found while processing mouse input!");
         }
     }
 }
