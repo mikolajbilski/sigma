7ee6cd21ee4666b16ef7823bd8c10422f8945f77
Mikolaj Bilski <mj.bilski@student.uw.edu.pl>
2024-12-16 02:13:44 +0100

    change pub to pub(crate)

--- src/mechanics/mod.rs
+++ src/mechanics/mod.rs
@@ -1,8 +1,8 @@
-pub mod found_set_event;
-pub mod game_manager;
-pub mod input_manager;
-pub mod playing_field;
-pub mod selection_manager;
-pub mod set;
-pub mod startup;
-pub mod timer;
+pub(crate) mod found_set_event;
+pub(crate) mod game_manager;
+pub(crate) mod input_manager;
+pub(crate) mod playing_field;
+pub(crate) mod selection_manager;
+pub(crate) mod set;
+pub(crate) mod startup;
+pub(crate) mod timer;
