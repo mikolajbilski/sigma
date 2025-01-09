d277050301f960b425962155ce204ebcb3567729
Mikolaj Bilski <mj.bilski@student.uw.edu.pl>
2025-01-09 22:11:53 +0100

    add menu spawning

--- src/ui/main_menu.rs
+++ src/ui/main_menu.rs
@@ -1,5 +1,7 @@
 use bevy::{color::Color, prelude::*};
 
+use super::button_markers::ButtonTypeMarker;
+
 const BUTTON_COLOR: Color = Color::srgb(0.15, 0.15, 0.15);
 
 pub(crate) fn spawn_menu(mut commands: Commands) {
@@ -14,35 +16,29 @@ pub(crate) fn spawn_menu(mut commands: Commands) {
         ..default()
     };
 
-    let start_game_button = ButtonBundle {
-        style: Style {
-            width: Val::Px(300.0),
-            height: Val::Px(80.0),
-            border: UiRect::all(Val::Px(5.0)),
-            justify_content: JustifyContent::Center,
-            align_items: AlignItems::Center,
-            ..default()
-        },
-        border_color: BorderColor(Color::BLACK),
-        background_color: BUTTON_COLOR.into(),
-        ..default()
-    };
+    let start_game_button = generate_button(ButtonTypeMarker::StartGame);
+    let stats_button = generate_button(ButtonTypeMarker::DisplayStats);
+    let exit_button = generate_button(ButtonTypeMarker::Exit);
 
-    let start_game_text_node = TextBundle::from_section(
-        "New Game",
-        TextStyle {
-            font_size: 40.0,
-            color: Color::srgb(0.9, 0.9, 0.9),
-            ..default()
-        },
-    );
+    let start_game_text = commands
+        .spawn(generate_button_bundle_text("Start Game"))
+        .id();
+    let stats_text = commands.spawn(generate_button_bundle_text("Stats")).id();
+    let exit_text = commands.spawn(generate_button_bundle_text("Exit")).id();
 
     let container = commands.spawn(container_node).id();
-    let button = commands.spawn(start_game_button).id();
-    let start_game_text = commands.spawn(start_game_text_node).id();
+    let start_game_button = commands.spawn(start_game_button).id();
+    let stats_button = commands.spawn(stats_button).id();
+    let exit_button = commands.spawn(exit_button).id();
 
-    commands.entity(button).push_children(&[start_game_text]);
-    commands.entity(container).push_children(&[button]);
+    commands
+        .entity(start_game_button)
+        .push_children(&[start_game_text]);
+    commands.entity(stats_button).push_children(&[stats_text]);
+    commands.entity(exit_button).push_children(&[exit_text]);
+    commands
+        .entity(container)
+        .push_children(&[start_game_button, stats_button, exit_button]);
 }
 
 pub(crate) fn main_menu_system(
@@ -62,4 +58,36 @@ pub(crate) fn main_menu_system(
             //TODO: Start a game
         }
     }
+}
+
+fn generate_button_bundle_text(content: &str) -> TextBundle {
+    TextBundle::from_section(
+        content,
+        TextStyle {
+            font_size: 40.0,
+            color: Color::srgb(0.9, 0.9, 0.9),
+            ..default()
+        },
+    )
+}
+
+fn generate_button(
+    buton_type: ButtonTypeMarker,
+) -> (ButtonBundle, ButtonTypeMarker) {
+    (
+        ButtonBundle {
+            style: Style {
+                width: Val::Px(300.0),
+                height: Val::Px(80.0),
+                border: UiRect::all(Val::Px(5.0)),
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                ..default()
+            },
+            border_color: BorderColor(Color::BLACK),
+            background_color: BUTTON_COLOR.into(),
+            ..default()
+        },
+        buton_type
+    )
 }
