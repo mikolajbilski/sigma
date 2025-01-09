28fc867bbbebed61f49107b48586da7c231d2864
Mikolaj Bilski <mj.bilski@student.uw.edu.pl>
2025-01-09 05:12:13 +0100

    timer now stores game time

--- src/mechanics/timer.rs
+++ src/mechanics/timer.rs
@@ -1,10 +1,12 @@
 use bevy::prelude::*;
+use std::time::Duration;
 
 use super::game_manager::GameEndedEvent;
 
 #[derive(Component)]
 pub(crate) struct TimerInfo {
     running: bool,
+    time: Duration,
 }
 
 pub(crate) fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
@@ -21,7 +23,7 @@ pub(crate) fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             transform: Transform::from_xyz(200.0, 800.0, 0.0),
             ..Default::default()
         })
-        .insert(TimerInfo { running: true });
+        .insert(TimerInfo { running: true, time: Duration::new(0, 0) });
 }
 
 pub(crate) fn update_timer(mut query: Query<(&mut Text, &mut TimerInfo)>, time: Res<Time>) {
@@ -32,12 +34,13 @@ pub(crate) fn update_timer(mut query: Query<(&mut Text, &mut TimerInfo)>, time: 
     let seconds = elapsed_time.as_secs() % 60;
     let hundredths = (elapsed_time.as_millis() % 1000) / 10;
 
-    for (mut text, timer) in query.iter_mut() {
+    for (mut text, mut timer) in query.iter_mut() {
         if timer.running {
             text.sections[0].value = format!(
                 "{:02}:{:02}:{:02}.{:02}",
                 hours, minutes, seconds, hundredths
             );
+            timer.time = elapsed_time;
         }
     }
 }
