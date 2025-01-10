598ebd62af5275c299050c1033164c1763a63a2c
Mikolaj Bilski <mj.bilski@student.uw.edu.pl>
2025-01-10 01:31:52 +0100

    implement displaying of stats table and number of games plated

--- src/ui/stats_screen.rs
+++ src/ui/stats_screen.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 
-use crate::states::AppState;
+use crate::{save::saving::load_top_scores, score_tracking::stats::Stats, states::AppState};
 
 use super::{
     button_markers::ButtonTypeMarker,
@@ -26,6 +26,12 @@ pub(crate) fn spawn_stats(mut commands: Commands) {
         StatsMarker {},
     );
 
+    let stats = load_top_scores();
+
+    let games_played_info = generate_games_played_text(&stats);
+
+    let stats_table = create_stats_table(&stats);
+
     let go_back_button = generate_button(ButtonTypeMarker::MainMenu);
 
     let go_back_text = commands
@@ -33,12 +39,14 @@ pub(crate) fn spawn_stats(mut commands: Commands) {
         .id();
 
     let container = commands.spawn(container_node).id();
+    let games_played_info = commands.spawn(games_played_info).id();
+    let stats_table = commands.spawn(stats_table).id();
     let go_back_button = commands.spawn(go_back_button).id();
 
     commands
         .entity(go_back_button)
         .push_children(&[go_back_text]);
-    commands.entity(container).push_children(&[go_back_button]);
+    commands.entity(container).push_children(&[games_played_info, stats_table, go_back_button]);
 }
 
 pub(crate) fn stats_system(
@@ -67,4 +75,19 @@ pub(crate) fn destroy_stats(
     for menu_item in &mut menu_query {
         commands.entity(menu_item).despawn_recursive();
     }
+}
+
+fn create_stats_table(stats: &Stats) -> NodeBundle {
+    todo!()
+}
+
+fn generate_games_played_text(stats: &Stats) -> TextBundle {
+    TextBundle::from_section(
+        format!("You have played {} games", stats.get_played_games()),
+        TextStyle {
+            font_size: 40.0,
+            color: Color::srgb(0.9, 0.9, 0.9),
+            ..default()
+        },
+    )
 }
