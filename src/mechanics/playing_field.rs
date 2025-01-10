7bfb4ea9323c1ebd06b8c00e0aa4a58885b40057
Mikolaj Bilski <mj.bilski@student.uw.edu.pl>
2025-01-10 01:54:41 +0100

    remove "cheating" printlns

--- src/mechanics/playing_field.rs
+++ src/mechanics/playing_field.rs
@@ -203,7 +203,6 @@ impl PlayingField {
                         (self.cards[i], self.cards[j], self.cards[k])
                     {
                         if is_set(&card1, &card2, &card3) {
-                            println!("SET:\n{},\n{},\n{}", card1, card2, card3);
                             return true;
                         }
                     }
